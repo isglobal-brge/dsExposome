0ff5fefa2ffdfe0793414fc87e075ecfe97c2b43
ESCRI11 <escri11@yahoo.es>
2020-08-26 12:57:18 +0200

    added input argument to exposures_pData

--- R/exposures_pData.R
+++ R/exposures_pData.R
@@ -4,15 +4,28 @@
 #' Exposome Set
 #'
 #' @param exposomeSet  \code{ExposomeSet} Exposome Set object
+#' @param target \code{character} To specify target of output table, \code{all} to include exposures and phenotypes,
+#' \code{exposures} to include only the exposures and \code{phenotypes} to include only the phenotypes. Default \code{all}
 #'
 #' @return \code{data.frame} With exposures and phenotypes
 #' @import rexposome, Biobase
 #'
 
-exposures_pData <- function(exposomeSet) {
+exposures_pData <- function(exposomeSet, target = "all") {
 
     # amb ds.cbind no funcione ¿????¿?¿
-  data <- cbind(rexposome::expos(exposomeSet), Biobase::pData(exposomeSet))
+  if(target == "all"){
+    data <- cbind(rexposome::expos(exposomeSet), Biobase::pData(exposomeSet))
+  }
+  else if(target == "exposures"){
+    data <- rexposome::expos(exposomeSet)
+  }
+  else if(target == "phenotypes"){
+    data <- Biobase::pData(exposomeSet)
+  }
+  
+  else{stop("Incorrect 'target' argument. Options: 'all', 'exposures' and 'phenotypes'", call.=FALSE)}
+  
   return(data)
 
 }
