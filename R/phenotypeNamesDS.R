b6e83ee1f1b5458b3d2ade941b6f9d9dafcf62a3
ESCRI11 <escri11@yahoo.es>
2020-08-27 13:16:20 +0200

    added phenotypeNamesDS function + documentation

---
+++ R/phenotypeNamesDS.R
@@ -0,0 +1,14 @@
+#' @title Exposure Phenotypes
+#' 
+#' @description Extract the phenotypes names from a Expression Set object
+#'
+#' @param exposomeSet \code{ExposomeSet} Exposome Set object
+#'
+#' @return \code{character vector} of the phenotypes names
+#' @import rexposome
+#'
+
+phenotypeNamesDS <- function(exposomeSet){
+  data <- rexposome::phenotypeNames(exposomeSet)
+  return(data)
+}
