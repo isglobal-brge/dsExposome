4d13cbe79e7369a2fb403aaaa2f47c2368ab935e
ESCRI11 <escri11@yahoo.es>
2020-08-31 11:45:26 +0200

    new functions + documentation

---
+++ R/scaleDS.R
@@ -0,0 +1,14 @@
+#' @title Scale a data frame
+#' 
+#'
+#' @param x \code{data frame} Table to be scaled
+#' @param center Corresponds to the \code{center} parameter of the base function \code{scale}
+#' @param scale Corresponds to the \code{scale} parameter of the base function \code{scale}
+#'
+#' @return \code{data frame} Scaled table
+#' 
+
+scaleDS <- function(x, center = TRUE, scale = TRUE){
+  data <- base::scale(x, center, scale)
+  return(data)
+}
