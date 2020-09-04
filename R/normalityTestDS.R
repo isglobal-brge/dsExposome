e9586488e33ebac7722000074f894e0b8c398a42
ESCRI11 <escri11@yahoo.es>
2020-09-04 16:15:09 +0200

    normalityTestDS added roxygen documentation

--- R/normalityTestDS.R
+++ R/normalityTestDS.R
@@ -1,3 +1,19 @@
+#' @title Normality test of exposures of ExposomeSet
+#' 
+#' @description Perform a normality test on all the exposures of an ExposomeSet object
+#'
+#' @param object \code{ExposomeSet} Exposome Set object
+#' @param th \code{numeric} (default \code{0.05}) Threshold to considere an exposure to follow a normal distribution.
+#' @param min.val \code{numeric} (default \code{5}) Minimum number of values not missings to test the exposures.
+#' @param na.rm \code{bool} (default \code{TRUE}) Removes the NA values to test the normality on the exposure.
+#' @param warnings \code{bool} (default \code{TRUE}) Show warnings if required.
+#'
+#' @return list of \code{data.frame} (one for each study server) with \cr
+#' - exposure: \code{character} Name of the exposures \cr
+#' - normality: \code{bool} If exposure is normal (\code{TRUE}) or not (\code{FALSE}) \cr
+#' - p.value: \code{numeric} P-value of the Shapiro-Wilk Normality Test
+#'
+
 normalityTestDS <- function(object, th = 0.05, min.val = 5, na.rm = TRUE,
                             warnings = TRUE){
   norm_test <- rexposome::normalityTest(object, , th, min.val, na.rm, warnings)
