54ca36c41d442b5dcc8e325dff70e3cfde27c685
ESCRI11 <escri11@yahoo.es>
2020-09-08 11:19:47 +0200

    new function added

---
+++ R/familyNamesDS.R
@@ -0,0 +1,14 @@
+#' @title Get family names from Exposome Set
+#' 
+#' @param object \code{ExposomeSet} Exposome Set object
+#' @param by.exposure \code{bool} (default \code{FALSE}) If \code{TRUE} a vector labeled with each exposure 
+#' name will be returned with the family of each exposures. If \code{FALSE} a vector with the 
+#' (unique) name of the families of exposures will be returned.
+#'
+#' @return A \code{character vector} with the family names of the Exposome Set
+#' 
+
+familyNamesDS <- function(object, by.exposure = FALSE){
+  f_names <- rexposome::familyNames(object, by.exposure)
+  return(f_names)
+}
