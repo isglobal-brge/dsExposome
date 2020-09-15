a6e0a82f805164a9e72c1dc13deac15c1b0ceb85
ESCRI11 <escri11@yahoo.es>
2020-09-15 14:26:51 +0200

    updated output

--- R/boxPlotGGDS.R
+++ R/boxPlotGGDS.R
@@ -1,5 +1,4 @@
 boxPlotGGDS <- function(data_table, group, group2){
-
   if (!is.null(group)) {
     if(!is.null(group2)) {
       plot_ret <- ggplot2::ggplot(data_table, ggplot2::aes_string(x = "x", y = "value", fill = "group")) +
@@ -21,16 +20,20 @@ boxPlotGGDS <- function(data_table, group, group2){
 
   if(!is.null(group) & is.null(group2)){
     return(list(plt$data[[1]][, !names(plt$data[[1]]) %in% c("outliers")], unique(data_table$x),
-                unique(data_table$group), "single_group"))
+                unique(data_table$group),
+                counts = dplyr::count(data_table, x, group),
+                "single_group"))
   }
   else if(!is.null(group) & !is.null(group2)){
     return(list(plt$data[[1]][, !names(plt$data[[1]]) %in% c("outliers")], unique(data_table$x),
                 unique(data_table$group), 
                 unique(data_table$group2),
+                counts = dplyr::count(data_table, x, group, group2),
                 "double_group"))
   }
   else{
     return(list(plt$data[[1]][, !names(plt$data[[1]]) %in% c("outliers")], unique(data_table$x),
+                counts = dplyr::count(data_table, x),
                 "no_group"))
   }
   
