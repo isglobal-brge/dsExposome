d5440d9c2cb21684845c91b9d9d9ccaca4a37e04
ESCRI11 <escri11@yahoo.es>
2020-09-15 16:33:52 +0200

    avoid spaces in fam names

--- R/exposomeSubsetDS.R
+++ R/exposomeSubsetDS.R
@@ -1,9 +1,9 @@
 exposomeSubsetDS <- function(set, fam){
   
-  mask <- Biobase::fData(set)$Family==fam[1]
+  mask <- Biobase::fData(set)$Family==stringr::str_replace_all(fam[1], " ", "")
   if (length(fam) > 1){
     for (i in 2:length(fam)) {
-      indic <- Biobase::fData(set)$Family==fam[i]
+      indic <- Biobase::fData(set)$Family==stringr::str_replace_all(fam[i], " ", "")
       mask <- mask | indic
     }
   }
