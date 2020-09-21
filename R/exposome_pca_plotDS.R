1261fbc647be2e61108c168dd52c4e5e4782c7b4
ESCRI11 <escri11@yahoo.es>
2020-09-21 16:30:34 +0200

    test5

--- R/exposome_pca_plotDS.R
+++ R/exposome_pca_plotDS.R
@@ -32,24 +32,24 @@ exposome_pca_plotDS <- function(object, set, phenotype){
         panel.grid.minor = ggplot2::element_blank(),
         legend.title = ggplot2::element_blank()
       ) +
-      ggplot2::geom_hline(yintercept = 0, linetype = "dotdash", color = "red", size = 1) +
-      ggplot2::geom_vline(xintercept = 0, linetype = "dotdash", color = "red", size = 1) +
+      # ggplot2::geom_hline(yintercept = 0, linetype = "dotdash", color = "red", size = 1) +
+      # ggplot2::geom_vline(xintercept = 0, linetype = "dotdash", color = "red", size = 1) +
       ggplot2::geom_point(ggplot2::aes_string(color = "Family")) +
-      ggplot2::xlab(paste0("PC", cmpX, " (", round(rexposome::extract(object, table="eigen")[cmpX, 2], 2), "%)")) +
-      ggplot2::ylab(paste0("PC", cmpY, " (", round(rexposome::extract(object, table="eigen")[cmpY, 2], 2), "%)"))
-    if(show.exposures) {
-      ## Add labels for features
-      plt <- plt + ggrepel::geom_text_repel(
-        data = dta,
-        ggplot2::aes_string(paste0("Dim.", cmpX), paste0("Dim.", cmpY), label="Label"),
-        size = 2,
-        # box.padding = ggplot2::unit(0.35, "lines"),
-        # point.padding = ggplot2::unit(0.3, "lines"),
-        color="#222222",
-        segment.color="#BBBBBB"
-      ) + ggplot2::theme_bw() + ggplot2::theme(legend.position="none")
-      ## /
-    }
+      # ggplot2::xlab(paste0("PC", cmpX, " (", round(rexposome::extract(object, table="eigen")[cmpX, 2], 2), "%)")) +
+      # ggplot2::ylab(paste0("PC", cmpY, " (", round(rexposome::extract(object, table="eigen")[cmpY, 2], 2), "%)"))
+    # if(show.exposures) {
+    #   ## Add labels for features
+    #   plt <- plt + ggrepel::geom_text_repel(
+    #     data = dta,
+    #     ggplot2::aes_string(paste0("Dim.", cmpX), paste0("Dim.", cmpY), label="Label"),
+    #     size = 2,
+    #     box.padding = ggplot2::unit(0.35, "lines"),
+    #     point.padding = ggplot2::unit(0.3, "lines"),
+    #     color="#222222",
+    #     segment.color="#BBBBBB"
+    #   ) + ggplot2::theme_bw() + ggplot2::theme(legend.position="none")
+    #   ## /
+    # }
     return(ggplot2::ggplot_build(plt))
   }
   
