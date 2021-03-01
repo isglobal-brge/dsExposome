764ec7893f72d63367d73de4f2f2872faf536c3e
Xavier Escribà Montagut <xavier.escriba@isglobal.org>
2021-03-01 13:57:38 +0100

    added inv exwas

---
+++ R/invExWASDS.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param object 
+#' @param formula 
+#' @param tef 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+invExWASDS <- function(object, formula, tef){
+  rexposome::invExWAS(object = object, formula = formula, tef = tef)
+}
