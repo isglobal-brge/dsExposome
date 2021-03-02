308e31e96eb7689c0c208d0e813600eb640ddf72
Xavier Escribà Montagut <xavier.escriba@isglobal.org>
2021-03-02 10:28:14 +0100

    invExwas added

--- R/invExWASDS.R
+++ R/invExWASDS.R
@@ -1,16 +1,16 @@
 #' Title
 #'
-#' @param object 
-#' @param formula 
-#' @param tef 
+#' @param object \code{ExposomeSet} Exposome object
+#' @param phenotype \code{character} Phenotype objective
+#' @param tef \code{bool} If \code{TRUE} computes the threshold for effective tests.
+#' @param ... \code{character vector} Adjusting phenotype covariables
 #'
-#' @return
+#' @return \code{ExWAS} object. With tef, coefficients and pvalues of associations
 #' @export
-#'
-#' @examples
+
 invExWASDS <- function(object, phenotype, tef, ...){
   covariables <- unlist(list(...))
-  formula <- paste0("~ ", phenotype, if(missing(covariables)){}else{paste(" +",paste0(covariables, collapse = " + "))})
+  formula <- paste0("~ ", phenotype, if(is.null(covariables)){}else{paste(" +",paste0(covariables, collapse = " + "))})
   formula <- as.formula(formula)
   inv_exwas <- rexposome::invExWAS(object = object, formula = formula, tef = tef)
   return(inv_exwas)
