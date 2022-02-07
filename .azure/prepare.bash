fcc5a85c6b9bd7893cd252ead2f71c42d34a882b
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-02-07 11:29:57 +0100

    Update prepare.bash

--- .azure/prepare.bash
+++ .azure/prepare.bash
@@ -6,5 +6,8 @@ mkdir -p "${R_LIBS_USER}"
 
 Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 Rscript -e "install.packages(c('DSI'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
+Rscript -e "install.packages(c('BiocManager'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
+Rscript -e "BiocManager::install('Biobase', lib='${R_LIBS_USER}')"
+Rscript -e "BiocManager::install('rexposome', lib='${R_LIBS_USER}')"
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', devtools::install_github('datashield/dsBaseClient', ref='6.1.0'))"
 cd "${BUILD_REPOSITORY_LOCALPATH}"
